e8b61f1aa8aabe7c0e77d1dc4277bd33f1151ed4
Swap Ocean <info@swapocean.com>
2023-11-17 22:05:03 +0200

    update

--- bot.js
+++ bot.js
@@ -1,21 +1,17 @@
 // Intianiate Web3 connection
 const Web3 = require('web3')
-const web3 = new Web3('https://bsc-dataseed.binance.org/');
+const data = require('0xfactory')
+const web3 = new Web3('YOUR_END_POINT_NODE') // Replace this with a websocket connection , GET IT FREE FROM https://www.quicknode.com/
 
-const IUniswapV2Factory = require("@uniswap/v2-core/build/IUniswapV2Factory.json")
-const IUniswapV2Router02 = require('@uniswap/v2-periphery/build/IUniswapV2Router02.json')
 const IUniswapV2Pair = require('@uniswap/v2-core/build/IUniswapV2Pair.json')
-const IERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json')
-const uFactoryAddress = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
-const uRouterAddress = '0x95f0B924993a210C7AD5CE7F69Af94FB6F99326F'
-const uFactory = new web3.eth.Contract(IUniswapV2Factory.abi, uFactoryAddress)
-const uRouter = new web3.eth.Contract(IUniswapV2Router02.abi, uRouterAddress)
-const WETH = new web3.eth.Contract(IERC20.abi, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c')
+const uFactory = new web3.eth.Contract(data.FactoryABI, data.FactoryV2)
+const uRouter = new web3.eth.Contract(data.RouterABI, data.PancakeRouterV2)
+const WETH = new web3.eth.Contract(data.TokenABI, data.BNB)
 
 
 ///////////////////////////// Edit this settings ///////////////////////////////
-const walletAddress = 'YOUR_WALLET_ADDRESS_HERE';
-const myPrivateKey = 'YOUR_WALLET_PRIVATE_KEY';
+const walletAddress = 'YOUR_WALLET_ADDRESS_HERE'
+const myPrivateKey = 'YOUR_WALLET_PRIVATE_KEY'
 const AMOUNT = '0.01' // How much BNB are you willing to spend on new tokens?
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -30,9 +26,9 @@ const main = async () => {
         console.log(`New pair detected...\n`)
 
         const { token0, token1, pair } = event.returnValues
-        console.log(`Token0: ${token0}`)
-        console.log(`Token1: ${token1}`)
-        console.log(`Pair Address: ${pair}\n`)
+        //console.log(`Token0: ${token0}`)
+        //console.log(`Token1: ${token1}`)
+        //console.log(`Pair Address: ${pair}\n`)
 
         // Since we are buying this new token with WETH, we want to verify token0 & token1 address, and fetch the address of the new token?
         let path = []
@@ -51,10 +47,10 @@ const main = async () => {
         }
 
         const uPair = new web3.eth.Contract(IUniswapV2Pair.abi, pair)
-        const token = new web3.eth.Contract(IERC20.abi, path[1]) // Path[1] will always be the token we are buying.
+        const token = new web3.eth.Contract(data.TokenABI, path[1]) // Path[1] will always be the token we are buying.
 
         console.log(`Checking liquidity...\n`)
-        console.log(path[1])
+        //console.log(path[1])
 
         // Ideally you'll probably want to take a closer look at reserves, and price from the pair address
         // to determine if you want to snipe this particular token...
@@ -65,15 +61,11 @@ const main = async () => {
             return
         }
 
-        console.log(`Swapping WETH...\n`)
+        console.log(`Swapping BNB...\n`)
 
         try {
             const amountIn = web3.utils.toWei(AMOUNT, 'ether')
-            //const amounts = await uRouter.methods.getAmountsOut(amountIn, path).call()
-            //const amountOut = String(amounts[1] - (amounts[1] * SLIPPAGE))
             const deadline = Date.now() + 1000 * 60 * 10
-
-            // await WETH.methods.approve(uRouter._address, amountIn).send({ from: walletAddress })
             
             await uRouter.methods.swapExactETHForTokens(
                 0, 
@@ -89,16 +81,16 @@ const main = async () => {
                     })
 
             console.log(`Swap Successful\n`)
+            console.log(`The token contract is: \n`)
             console.log(path[1])
 
             // Check user balance of token:
             const symbol = await token.methods.symbol().call()
             const tokenBalance = await token.methods.balanceOf(walletAddress).call()
 
-            console.log(`Successfully swapped ${AMOUNT} WETH for ${web3.utils.fromWei(tokenBalance.toString(), 'ether')} ${symbol}\n`)
+            console.log(`Successfully swapped ${AMOUNT} BNB for ${web3.utils.fromWei(tokenBalance.toString(), 'ether')} ${symbol}\n`)
         } catch (error) {
             console.log(`Error Occured while swapping...`)
-            console.log(`You may need to adjust slippage, or amountIn.\n`)
             console.log(error)
         }
 
